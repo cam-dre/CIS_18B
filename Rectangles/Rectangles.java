426a47dbf67d681dc5926311db6931dc16e12a14
Cameron Perry <camperry@sbcglobal.net>
2018-03-20 21:43:58 -0700

    Add files via upload

--- Rectangles/Rectangles.java
+++ Rectangles/Rectangles.java
@@ -9,46 +9,46 @@ public class Rectangles extends JApplet {
 	Integer width;
 	
 	public void init() {
-			while (length == null) {
-				try {
+		while (length == null) {
+			try {
+				lengthStr = JOptionPane.showInputDialog(null, "Enter the length of the rectangle.", "Length", JOptionPane.PLAIN_MESSAGE);
+				if (lengthStr == null)
+					System.exit(0);
+				length = Integer.parseInt(lengthStr);
+					
+				while (length < 0) {
+					JOptionPane.showMessageDialog(null, "INVALID ENTRY!", "ERROR", JOptionPane.ERROR_MESSAGE);
 					lengthStr = JOptionPane.showInputDialog(null, "Enter the length of the rectangle.", "Length", JOptionPane.PLAIN_MESSAGE);
 					if (lengthStr == null)
 						System.exit(0);
-					length = Integer.parseInt(lengthStr);
-					
-					while (length < 0) {
-						JOptionPane.showMessageDialog(null, "INVALID ENTRY!", "ERROR", JOptionPane.ERROR_MESSAGE);
-						lengthStr = JOptionPane.showInputDialog(null, "Enter the length of the rectangle.", "Length", JOptionPane.PLAIN_MESSAGE);
-						if (lengthStr == null)
-							System.exit(0);
-						length = Integer.parseInt(lengthStr);	
-					}
-				}
-				catch (Exception e) {
-					JOptionPane.showMessageDialog(null, "INVALID ENTRY!", "ERROR", JOptionPane.ERROR_MESSAGE);;
-					length = null;
+					length = Integer.parseInt(lengthStr);	
 				}
 			}
+			catch (Exception e) {
+				JOptionPane.showMessageDialog(null, "INVALID ENTRY!", "ERROR", JOptionPane.ERROR_MESSAGE);;
+				length = null;
+			}
+		}
 		
 		while (width == null) {
-				try {
+			try {
+				widthStr = JOptionPane.showInputDialog(null, "Enter the width of the rectangle.", "Width", JOptionPane.PLAIN_MESSAGE);
+				if (widthStr == null)
+					System.exit(0);
+				width = Integer.parseInt(widthStr);
+					
+				while (width < 0) {
+					JOptionPane.showMessageDialog(null, "INVALID ENTRY!", "ERROR", JOptionPane.ERROR_MESSAGE);
 					widthStr = JOptionPane.showInputDialog(null, "Enter the width of the rectangle.", "Width", JOptionPane.PLAIN_MESSAGE);
 					if (widthStr == null)
 						System.exit(0);
 					width = Integer.parseInt(widthStr);
-					
-					while (width < 0) {
-						JOptionPane.showMessageDialog(null, "INVALID ENTRY!", "ERROR", JOptionPane.ERROR_MESSAGE);
-						widthStr = JOptionPane.showInputDialog(null, "Enter the width of the rectangle.", "Width", JOptionPane.PLAIN_MESSAGE);
-						if (widthStr == null)
-							System.exit(0);
-						width = Integer.parseInt(widthStr);
-					}
 				}
-				catch (Exception e) {
-					JOptionPane.showMessageDialog(null, "INVALID ENTRY!", "ERROR", JOptionPane.ERROR_MESSAGE);
-					width = null;
-				}
+			}
+			catch (Exception e) {
+				JOptionPane.showMessageDialog(null, "INVALID ENTRY!", "ERROR", JOptionPane.ERROR_MESSAGE);
+				width = null;
+			}
 		}
 	}
 	
